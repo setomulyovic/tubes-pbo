10c888d082877ced19727d182a2e91f90d1c24ab
setomulyovic <32457267+setomulyovic@users.noreply.github.com>
2017-11-11 18:31:52 +0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,40 @@
+
+public abstract class Employee {
+    private String name;
+    private String address;
+    private String gender;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String address, String gender) {
+        this.name = name;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
+    
